29d55c0fa99d4a0e85e8f9330f1cf9687494571f
root <root@Diaz>
2024-08-10 14:05:55 +0700

    drivers: misc: underclock GPU freq from 1100 to 1000 mhz & oV +50mV  Committer: root <yz-prjkt@diaspasa16@gmail.com>  Changes to be committed: 	modified:   drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h

--- drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
+++ drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
@@ -17,8 +17,8 @@
 /**************************************************
  * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG_GPU_DVFS_FREQ0			(1100000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ1			(995000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ0			(1000000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ1			(975000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ2			(950000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ3			(925000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ4			(900000)	/* KHz */
